0a37545af508c7469077c0d6fa8d569aa5f5eb78
selomac <selaytekgul@gmail.com>
2025-10-27 23:53:41 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    auto a = 1;
+    std::cin >> a;
+    std::cout << "my a: " << a << std::endl;
+    return 0;
+}
