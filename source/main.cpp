dc199366e261e6279d93cea8f49a04e86d1e590d
selomac <selaytekgul@gmail.com>
2025-10-29 22:53:40 +0300

    add bezier example

--- source/main.cpp
+++ source/main.cpp
@@ -1,32 +1,81 @@
-#include <iostream>
-#include <imgui.h>
-#include <rlImGui.h>
+// #include "raylib.h"
+// #include "raymath.h"
 #include "Bezier.h"
 
-int main()
+using namespace raylib;
+
+int main(void)
 {
-    // using namespace raylib;
-    raylib::InitWindow(800, 450, "raylib + ImGui + rlImGui (minimal)");
-    raylib::SetTargetFPS(60);
+    Bezier b;
+    // --- Setup ---
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight, "Bézier Curve Math Demo");
+    SetTargetFPS(60);
 
-    raylib::rlImGuiSetup(true); // true = dark theme
+    // --- Control Points for Quadratic Curve ---
+    Vector2 q_p0 = { 100, (float)screenHeight / 2 };
+    Vector2 q_p1 = { 0, 0 }; // This one will follow the mouse
+    Vector2 q_p2 = { 400, (float)screenHeight / 2 };
 
-    bool show_demo = true;
-    while (!raylib::WindowShouldClose()) {
-        raylib::BeginDrawing();
-        raylib::ClearBackground(raylib::RAYWHITE);
+    // --- Control Points for Cubic Curve ---
+    Vector2 c_p0 = { 600, (float)screenHeight / 2 };
+    Vector2 c_p1 = { 700, 100 };
+    Vector2 c_p2 = { 900, (float)screenHeight - 100 };
+    Vector2 c_p3 = { 1100, (float)screenHeight / 2 };
 
-        // ImGui pass (between BeginDrawing/EndDrawing)
-        raylib::rlImGuiBegin();
-        ImGui::Begin("Hello"); ImGui::Text("Hi from ImGui + raylib!"); ImGui::End();
-        if (show_demo) ImGui::ShowDemoWindow(&show_demo); // optional
-        raylib::rlImGuiEnd();
 
-        raylib::DrawText("Raylib draw still works under the ImGui overlay.", 10, 420, 10, raylib::DARKGRAY);
-        raylib::EndDrawing();
+    // --- Main Game Loop ---
+    while (!WindowShouldClose())
+    {
+        // --- Update ---
+        // Make the quadratic control point follow the mouse
+        q_p1 = GetMousePosition();
+
+        // --- Draw ---
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        // --- Draw Quadratic Example ---
+        DrawText("Quadratic Bézier", 100, 50, 20, GRAY);
+
+        // 1. Draw the smooth curve
+        b.drawQuadraticCurve(q_p0, q_p1, q_p2, 3.0f, WHITE, 30);
+
+        // 2. Draw the control "legs"
+        DrawLineV(q_p0, q_p1, GRAY);
+        DrawLineV(q_p1, q_p2, GRAY);
+
+        // 3. Draw the control points
+        DrawCircleV(q_p0, 7, RED);
+        DrawCircleV(q_p1, 7, GREEN); // The one you move
+        DrawCircleV(q_p2, 7, RED);
+
+
+        // --- Draw Cubic Example ---
+        DrawText("Cubic Bézier", 600, 50, 20, GRAY);
+
+        // 1. Draw the smooth curve
+        b.drawCubicCurve(c_p0, c_p1, c_p2, c_p3, 3.0f, WHITE, 40);
+
+        // 2. Draw the control "legs"
+        DrawLineV(c_p0, c_p1, GRAY);
+        DrawLineV(c_p1, c_p2, GRAY);
+        DrawLineV(c_p2, c_p3, GRAY);
+
+        // 3. Draw the control points
+        DrawCircleV(c_p0, 7, BLUE);
+        DrawCircleV(c_p1, 7, YELLOW);
+        DrawCircleV(c_p2, 7, YELLOW);
+        DrawCircleV(c_p3, 7, BLUE);
+
+
+        DrawText("Move mouse to control the green point", 10, 10, 20, LIGHTGRAY);
+        DrawFPS(screenWidth - 100, 10);
+
+        EndDrawing();
     }
 
-    raylib::rlImGuiShutdown();
-    raylib::CloseWindow();
+    CloseWindow();
     return 0;
 }
--- source/math/Bezier.h
+++ source/math/Bezier.h
@@ -2,12 +2,19 @@
 // Created by Selay Tekgül on 29.10.2025.
 //
 
-#ifndef WASM_MINI_BEZIER_H
-#define WASM_MINI_BEZIER_H
+#pragma once
 
+#include "raylib.h"
 
+using namespace raylib;
 class Bezier
-{};
+{
+public:
+    Bezier() = default;
+    void drawQuadraticCurve(Vector2 p0, Vector2 p1, Vector2 p2, float thickness, Color color, int segments = 30);
+    void drawCubicCurve(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float thickness, Color color, int segments = 40);
+private:
+    Vector2 getQuadraticPoint(Vector2 p0, Vector2 p1, Vector2 p2, float t);
+    Vector2 getCubicPoint(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t);
 
-
-#endif //WASM_MINI_BEZIER_H
+};
